3bc8c5fcb979fe0be5320deb4c76a2d228caacd3
Kiarash Zamanifar <kiarashz@hotmail.com>
2020-02-09 16:29:01 +1100

    install docker via apt

--- ubuntu-19-post-install.sh
+++ ubuntu-19-post-install.sh
@@ -9,16 +9,17 @@ skd install maven
 skd install gradle
 sudo addgroup --system docker
 sudo usermod -aG docker $username
-sudo snap install docker
+sudo apt update && sudo apt install docker.io
 
 # below may need update over time
 cat > /tmp/daemon.json << EOF
 {
-    "log-level":        "error",
-    "storage-driver":   "overlay2",
     "insecure-registries" : ["172.30.0.0/0"]
 }
 EOF
-sudo mv /tmp/daemon.json /var/snap/docker/423/config/daemon.json
+sudo mv /tmp/daemon.json /etc/docker/daemon.json
 
-# install openshift
+# install openshift https://github.com/openshift/origin/releases
+curl -L -o /tmp/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz \ 
+https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+
