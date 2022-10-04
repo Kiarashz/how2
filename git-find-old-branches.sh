d43af41acd8ca4ecf7776b01aa6067205c72a9da
Kiarash Zamanifar <kiarashz@hotmail.com>
2022-10-04 15:58:11 +1100

    Update and rename git-find-old-branches.sh to git-useful-commands.sh

--- git-find-old-branches.sh
+++
@@ -1,1 +0,0 @@
-for branch in $(git branch -r --sort=committerdate | xargs echo); do echo ============= $branch =============; git log -1 --since='Jan 01, 2021' -s $branch; done
---
+++ git-useful-commands.sh
@@ -0,0 +1,15 @@
+# find local branches that do not exist on remote anymore
+for branch in $(git branch -r --sort=committerdate | xargs echo); do echo ============= $branch =============; git log -1 --since='Jan 01, 2021' -s $branch; done
+
+# find commit by message
+$ git log --all --grep='my commit message'
+commit 111111111111111111111111111111111111111
+Author: xxxxxxxxxxxxxxx
+Date:   xxxxxxxxxxxxxxxx
+. . .
+
+
+# find branch where commit was first made
+$ git name-rev --name-only --exclude=tags/*  111111111111111111111111111111111111111
+remotes/origin/BRANCH-WHERE-COMMIT-WAS-MADE
+
