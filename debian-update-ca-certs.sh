861db9f264f61c1f1ed74ebab97fc96d7bdf7d36
Kiarash Zamanifar <kiarashz@hotmail.com>
2022-11-18 16:03:22 +1100

    Update debian-update-ca-certs.sh

--- debian-update-ca-certs.sh
+++ debian-update-ca-certs.sh
@@ -2,3 +2,6 @@ mkdir -p /usr/share/ca-certificates/myCA
 sudo cp ca1.pem /usr/share/ca-certificates/myCA
 sudo echo "myCA/ca1.pem" >> /etc/ca-certificates.conf
 update-ca-certificates
+
+# to split ca files into separate crt files:
+cat multiple_cacerts.pem | awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "/usr/local/share/ca-certificates/my_cacert" n ".crt"}'
